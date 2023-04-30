2695279d6ce7b22633edddbadcd97e3887d61f55
Ryan Williams <git@ryan.nz>
2023-04-30 16:01:11 +1200

    Tweak grid drawing

--- src/shaders/grid.frag.glsl
+++ src/shaders/grid.frag.glsl
@@ -17,16 +17,16 @@ layout(location = 3) out vec4 outSpecular;
 float edgeDistance(vec3 barycentric, float thickness) {
 	vec3 dx = dFdx(barycentric);
 	vec3 dy = dFdy(barycentric);
-	vec3 d = sqrt(dx * dx + dy * dy);
-	vec3 a = step(d * thickness, barycentric);
+	vec3 d = sqrt(dx * dx + dy * dy) * thickness;
+	vec3 a = smoothstep(vec3(0.0), d, barycentric);
 	return min(min(a.x, a.y), a.z);
 }
 
 void main(void) {
-	vec3 c0 = vec3(1.0, 0.8, 0.1);
-	vec3 c1 = vec3(0.3, 0.1, 0.8);
-	vec3 color = mix(c0, c1, edgeDistance(vBarycentric, 1.0));
-	outAlbedo = vec4(color, 1.0);
+	vec4 lineColor = vec4(0.224, 0.722, 1.000, 1.0);
+	vec4 faceColor = vec4(0.051, 0.133, 0.286, 1.0);
+	vec4 color = mix(lineColor, faceColor, edgeDistance(vBarycentric, 2.0));
+	outAlbedo = color;
 	outPosition = vec4(vPosition, 1.0);
 	outNormal = vec4(vBarycentric, 1.0);
 	outSpecular = vec4(vTexCoord, 0.0, 1.0);
